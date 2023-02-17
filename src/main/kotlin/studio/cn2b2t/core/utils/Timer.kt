f7c5442b478f3e48d00c2f5d68a0c4ac9168a935
DrRockyMC <65335475+rocky-co@users.noreply.github.com>
2023-02-17 17:48:34 +0800

    Update Timer.kt

--- src/main/kotlin/studio/cn2b2t/core/utils/Timer.kt
+++ src/main/kotlin/studio/cn2b2t/core/utils/Timer.kt
@@ -1,21 +1,3 @@
 package studio.cn2b2t.core.utils
 
-class Timer {
-    private var lastTimer: Long = System.currentTimeMillis()
-
-    fun passedMs(ms: Long): Boolean {
-        return System.currentTimeMillis() - this.lastTimer >= ms
-    }
-
-    fun passedSec(sec: Long): Boolean {
-        return passedMs(sec * 1000)
-    }
-
-    fun passedMin(min: Int): Boolean {
-        return passedSec((min * 60).toLong())
-    }
-
-    fun reset() {
-        lastTimer = System.currentTimeMillis()
-    }
-}
+// protect
