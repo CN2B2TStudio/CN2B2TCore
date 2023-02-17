edc3ed9b47d9fa82cadbd96379bdb722e9740a36
DrRockyMC <65335475+rocky-co@users.noreply.github.com>
2023-02-17 17:49:25 +0800

    Update FlagUtil.kt

--- src/main/kotlin/studio/cn2b2t/core/utils/FlagUtil.kt
+++ src/main/kotlin/studio/cn2b2t/core/utils/FlagUtil.kt
@@ -7,22 +7,21 @@ import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
 
 fun flagWithEvent(player: CraftPlayer, location: Location?, event: PacketEvent, cancel: Boolean) {
-    event.isCancelled = cancel
-    sendToMainThread { val bl = player.teleport(location) }
+    // protect
 }
 
 fun flagNoEvent(player: CraftPlayer, location: Location?) {
-    sendToMainThread { val bl = player.teleport(location) }
+    // protect
 }
 
 fun flagNoEvent(player: Player, location: Location?) {
-    sendToMainThread { val bl = player.teleport(location) }
+    // protect
 }
 
 fun flagNoEvent(player: CraftPlayer, x: Double, y: Double, z: Double) {
-    sendToMainThread { val bl = player.teleport(Location(player.world, x, y, z)) }
+    // protect
 }
 
 fun flagNoEvent(player: CraftPlayer, x: Double, y: Double, z: Double, yaw: Float, pitch: Float) {
-    sendToMainThread { val bl = player.teleport(Location(player.world, x, y, z, yaw, pitch)) }
+    // protect
 }
